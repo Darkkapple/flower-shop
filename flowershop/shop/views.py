5530be24e430940238166ae0ddf492a74a8203a3
Seva and Sema <Agentcema00@gmail.com>
2024-06-23 13:59:18 +0600

    Перенёс код в index.html

--- flowershop/shop/views.py
+++ flowershop/shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Click me</a>")
+    return render(request,"index.html")
