1779da6efdfe9aef4b7a2075c74ce87e62ab3b9d
Seva and Sema <Agentcema00@gmail.com>
2024-09-06 17:43:24 +0600

    возможно конечная версия сайта

--- flowershop/shop/tests.py
+++ flowershop/shop/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
+<div>
+      <img src = "{{ product.image_url }}" class ="productImage" >
+      <div class ="productInfo">
+         < h1 > {{product.name}} < /h1 >
+         <h3> {{product.price}} ₽ </h3 >
+      </div>
+</div>
